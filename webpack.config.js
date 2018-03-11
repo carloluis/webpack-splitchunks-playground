2513b42827019440ba19da6e9ea245a6e30b0186
carloluis <carloluisr@gmail.com>
2018-03-11 18:10:43 -0300

    new webpack config to prevent duplicated code in chunks

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'none',
+	entry: {
+		core: './src/bundles/core.js',
+		coreB: './src/bundles/core-b.js',
+		a: './src/bundles/a.js',
+		b: './src/bundles/b.js'
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	optimization: {
+		runtimeChunk: 'single',
+
+		splitChunks: {
+			chunks: 'all',
+
+			cacheGroups: {
+				default: {
+					enforce: true,
+					priority: 1
+				},
+				vendors: {
+					test: /[\\/]node_modules[\\/]/,
+					priority: 2,
+					name: 'vendors',
+					enforce: true,
+					chunks: 'all'
+				}
+			}
+		}
+	}
+};
